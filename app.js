c6c1a78f32bab10ed89cab5cd1a447dc3613ffbd
DEMAZ <baptiste.desmasures@gmail.com>
2014-04-27 23:00:57 +0200

    sauvegarde all

--- app.js
+++ app.js
@@ -1,4 +1,119 @@
-var app1 = angular.module('main', ['ngTable']);
+var app1 = angular.module('main', ['ngRoute','ngTable','ngResource']).config(function($routeProvider, $locationProvider, $httpProvider) {
+    //================================================
+    // Check if the user is connected
+    //================================================
+    var checkLoggedin = function($q, $timeout, $http, $location, $rootScope){
+      // Initialize a new promise
+      var deferred = $q.defer();
+
+      // Make an AJAX call to check if the user is logged in
+      $http.get('http://localhost:8888/loggedin').success(function(logged){
+        // Authenticated
+    	  alert(logged);
+        if (logged == true)
+          $timeout(deferred.resolve, 0);
+
+        // Not Authenticated
+        else {
+          $rootScope.message = 'You need to log in.';
+          $timeout(function(){deferred.reject();}, 0);
+          window.location  ='/login.html';
+        }
+      });
+
+      return deferred.promise;
+    };
+    //================================================
+    
+    //================================================
+    // Add an interceptor for AJAX errors
+    //================================================
+    $httpProvider.responseInterceptors.push(function($q, $location) {
+      return function(promise) {
+        return promise.then(
+          // Success: just return the response
+          function(response){
+            return response;
+          }, 
+          // Error: check the error status to get only the 401
+          function(response) {
+            if (response.status === 401)
+              $location.url('/login.html');
+            return $q.reject(response);
+          }
+        );
+      }
+    });
+    //================================================
+
+    //================================================
+    // Define all the routes
+    //================================================
+    $routeProvider.
+ 	when('/projet', {
+     	templateUrl: 'partials/projet-liste.html',
+     	controller: 'compte',
+     	resolve: {
+     	          loggedin: checkLoggedin
+     	        }
+     	}).
+    when('/liste-url', {
+         	templateUrl: 'partials/projet-uri.html',
+         	controller: 'projet'
+         	}).
+ 	when('/create-projet', {
+ 	templateUrl: 'partials/projet-info.html',
+ 	controller: 'projet'
+ 	}).
+ 	when('/login', {
+     	templateUrl: 'login.html',
+     	controller: 'login'
+     	}).
+ 	otherwise({
+ 		redirectTo: '/projet'
+ 		});
+    //================================================
+
+  }) // end of config()
+  .run(function($rootScope, $http){
+    $rootScope.message = '';
+
+    // Logout function is available in any pages
+    $rootScope.logout = function(){
+      $rootScope.message = 'Logged out.';
+      $http.post('/logout');
+    };
+  });
+
+
+
+/*
+app1.config(['$routeProvider',
+         	function($routeProvider) {
+         	$routeProvider.
+         	when('/projet', {
+             	templateUrl: 'partials/projet-liste.html',
+             	controller: 'compte',
+             	resolve: {
+             	          loggedin: checkLoggedin
+             	        }
+             	}).
+            when('/liste-url', {
+                 	templateUrl: 'partials/projet-uri.html',
+                 	controller: 'projet'
+                 	}).
+         	when('/create-projet', {
+         	templateUrl: 'partials/projet-info.html',
+         	controller: 'projet'
+         	}).
+         	when('/login', {
+             	templateUrl: 'login.html',
+             	controller: 'login'
+             	}).
+         	otherwise({
+         		redirectTo: '/projet'
+         		});
+         }]);*/
 
 app1.controller('compte', function($http,$scope) {
 	ctrl = this;
@@ -30,6 +145,44 @@ app1.controller('liste', function($http,$scope) {
 		$scope.users = data;
       })
 })
+
+app1.controller('login', function($http,$scope) {
+	var app1 = this;
+    app1.setUser = function(user) { 
+	$http({
+            method : 'POST',
+            url : 'http://localhost:8888/login',
+            data : user
+        }).success(function(data, status, headers, config) {
+        		
+	        	if(data == 'true') {
+	        		app1.logSuccess();
+	        	} else {
+	        		app1.logFail();
+	        	}
+        	}).error(function(data, status, headers, config) {
+        		alert('error')
+        	});   
+    }
+    
+    $http.get("http://localhost:8888/loggedin")
+    .success(function(data) {
+		$scope.users = data;
+    })
+    
+    app1.logFail = function() { 
+       	alert('log fail')
+    }
+    
+    app1.logSuccess = function() { 
+    	alert('log succes');
+    	//window.location  = '/projet.html';
+    }
+    
+    
+})
+
+
 
 
 
--- exemple.js
+++ exemple.js
@@ -1,34 +1,106 @@
 var express = require('express');
 var http = require('http');
+var path = require('path');
 var cors = require('cors');
 var connect = require('connect');
+var passport = require('passport');
+var LocalStrategy = require('passport-local').Strategy;
+
+//==================================================================
+//Define the strategy to be used by PassportJS
+passport.use(new LocalStrategy(
+function(username, password, done) {
+ if (username === "admin" && password === "admin") // stupid example
+   return done(null, {name: "admin"});
+
+ return done(null, false, { message: 'Incorrect username.' });
+}
+));
 
 var app = express();
-app.use(connect.urlencoded())
-app.use(connect.json())
+/*app.use(express.cookieParser());
+app.use(express.session({ secret: 'securedsession' }));
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
+app.use(connect.urlencoded());
+app.use(connect.json());
 app.use(cors());
-app.set('port',8888);
+app.set('port',process.env.PORT || 8888);*/
 
-http.createServer(app).listen(app.get('port'), function(){
-  console.log('Express server listening on port ' + app.get('port'));
+app.set('port', process.env.PORT || 8888);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+app.use(express.favicon());
+app.use(express.logger('dev'));
+app.use(express.cookieParser()); 
+app.use(express.bodyParser());
+app.use(express.methodOverride());
+app.use(connect.urlencoded());
+app.use(connect.json());
+app.use(cors());
+app.use(express.session({ secret: 'securedsession' }));
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
+app.use(app.router);
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Serialized and deserialized methods when got from session
+passport.serializeUser(function(user, done) {
+    done(null, user);
+});
+
+passport.deserializeUser(function(user, done) {
+    done(null, user);
 });
 
 
-var data = [{"name": "baptiste","last":"desmaz"},{"name": "abby","last":"lam"}];
+var users = [{"name": "baptiste","password":"demaz"},{"name": "abby","password":"lam"}];
+
+var listeUrls = [{"uri": "jean","codeRetour":"200"},{"uri": "pantalon","codeRetour":"404"}];
 
 app.get('/users', function(req,res) {
-res.send(data);
+res.send(users);
 });
 
+app.get('/liste', function(req,res) {
+	res.send(listeUrls);
+	});
+
 app.post('/users', function(req, res) {
-	data.push(req.body);
-    res.send(data);
+	users.push(req.body);
+    res.send(users);
 });
 
 app.post('/createProject', function(req, res) {
 	console.log('creation projet');
 	res.send('');
 });
+
+app.post('/login', function(req, res) {
+	console.log('debug : ' + req.session.islog);
+	req.session.islog = false;
+	for(var i=0;i < users.length;i++){ 
+		console.log('name : '+users[i].name + ' pwd : '+ users[i].password);
+		if(users[i].name == req.param('username')  
+				&& users[i].password == req.param('password'))	{
+			req.session.islog = true;
+		console.log('user ' + users[i].name +' is authentified'); 
+		}
+	}
+	res.send(req.session.islog);
+});
+
+app.get('/loggedin', function(req,res) {
+	console.log('Is log ? : ' + req.session.islog); 
+		res.send(req.session.islog);
+	});
+
+
+http.createServer(app).listen(app.get('port'), function(){
+	  console.log('Express server listening on port ' + app.get('port'));
+	});
+
+
 
 
 
